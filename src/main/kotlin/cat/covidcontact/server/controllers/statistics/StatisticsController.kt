67c4ba7c1d357923bde527e297f6c18d75cf22f1
Albert Pinto <apintogil@gmail.com>
2021-06-20 12:30:00 +0200

    Add new location url.

--- src/main/kotlin/cat/covidcontact/server/controllers/statistics/StatisticsController.kt
+++ src/main/kotlin/cat/covidcontact/server/controllers/statistics/StatisticsController.kt
@@ -3,7 +3,6 @@ package cat.covidcontact.server.controllers.statistics
 import cat.covidcontact.server.controllers.runGet
 import cat.covidcontact.server.model.nodes.user.Gender
 import cat.covidcontact.server.model.post.PostUserInteractionsStatistics
-import cat.covidcontact.server.services.location.LocationService
 import cat.covidcontact.server.services.statistics.StatisticsService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(StatisticsControllerUrls.BASE)
 class StatisticsController(
     private val statisticsService: StatisticsService,
-    private val locationService: LocationService
 ) {
 
     @GetMapping(StatisticsControllerUrls.USER_INTERACTIONS)
